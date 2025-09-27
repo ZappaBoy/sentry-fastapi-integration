16f9f5cc55dc0c2bfec7073dc78673540e33185c
Federico Zappone <federico.zappone@justanother.cloud>
2025-09-27 02:45:43 +0200

    Add base configuration files

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-# Fastapi template
+# Sentry and FastAPI integration
 
-# Create `.env`
+## Create `.env`
 ```shell
 ENVIRONMENT=development
-PORT=3003
+PORT=3000
 LOG_LEVEL=debug
 ALLOWED_HOSTS=localhost,otherlocalhost
+```
+
+## Deploy Sentry
+
+```shell
+VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/getsentry/self-hosted/releases/latest)
+VERSION=${VERSION##*/}
+git clone https://github.com/getsentry/self-hosted.git
+cd self-hosted
+git checkout ${VERSION}
+
+# During the installation an accout will be created for you.
+./install.sh
+# After installation, run the following to start Sentry:
+docker compose up --wait
+```
+
+If you want to create a new user, run:
+
+```shell
+docker compose run --rm web createuser
 ```
--- app/api.py
+++ app/api.py
@@ -1,7 +1,34 @@
-from fastapi import APIRouter
+import sentry_sdk
+from fastapi import APIRouter, FastAPI
 
 from services.core import Core
 from utils.logger import Logger
+
+sentry_sdk.init(
+    dsn="http://your_dsn@your_sentry_server/your_project_id",
+    send_default_pii=True,
+    # traces_sample_rate=0.5,
+    # # enable or disable profiling or logs if desired
+    # integrations=[
+    #     FastApiIntegration(
+    #         transaction_style="endpoint",
+    #         failed_request_status_codes={403, *range(500, 600)},
+    #         http_methods_to_capture=("GET", "POST", "PUT"),
+    #     ),
+    #     StarletteIntegration(
+    #         transaction_style="endpoint",
+    #         failed_request_status_codes={403, *range(500, 600)},
+    #         http_methods_to_capture=("GET", "POST", "PUT"),
+    #     ),
+    # ],
+)
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def ping():
+    return {"status": "ok"}
 
 router = APIRouter()
 core = Core()
@@ -13,7 +40,6 @@ def healthcheck():
     return {"Status": "Alive"}
 
 
-@router.get("/test")
-def test():
-    status = core.test()
-    return {"Status": status}
+@router.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
